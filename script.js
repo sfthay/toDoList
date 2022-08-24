618e79fb35efcde5b88c2ebe1f8a931633973414
Thaynara Maciel <thay.maciel7@gmail.com>
2022-08-23 23:05:00 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+const taskInput = document.querySelector(".task-input input"),
+taskBox = document.querySelector(".task-box");
+
+let todos = JSON.parse(localStorage.getItem("todo-list"));
+
+function showTodo() {
+    let li = "";
+    if(todos) {
+        todos.forEach((todo, id) => {
+        li += `<li class="task">
+                <label for="${id}">
+                    <input onclick = "updateStatus(this)" type="checkbox" id="${id}">
+                    <p>${todo.name}</p>
+                </label>
+                <div class="settings">
+                    <i class="uil uil-ellipsis-h"></i>
+                    <ul class="task-menu">
+                        <li><i class="uil uil-pen"></i>Edit</li>
+                        <li><i class="uil uil-trash"></i>Delete</li>
+                    </ul>
+                </div>
+            </li>`;
+
+    });
+    }
+    taskBox.innerHTML = li;
+}
+
+showTodo();
+
+function updateStatus(selectedTask) {
+    let taskName = selectedTask.parentElement.lastElementChild;
+    if(selectedTask.checked) {
+        taskName.classList.add("checked");
+        todos[selectedTask.id].status = "completed";
+    } else {
+        taskName.classList.remove("checked");
+        todos[selectedTask.id].status = "pending";
+    }
+    localStorage.setItem("todo-list", JSON.stringify(todos));
+}
+
+
+taskInput.addEventListener("keyup", e => {
+    let userTask = taskInput.value.trim();
+    if (e.key == "Enter" && userTask) {
+        if (!todos) {  //if todos doesn't exist, pass an empty array to todos
+            todos = [];
+        }
+        taskInput.value = "";
+        let taskInfo = { name: userTask, status: "pending" };
+        todos.push(taskInfo); //adding new tasks to todos
+        localStorage.setItem("todo-list", JSON.stringify(todos));
+        showTodo();
+    }
+});
